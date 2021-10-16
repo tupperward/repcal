88ebffbf0ea10edb6bbf6b3c9f06f341b6c32547
tupperward <tupperward@gmail.com>
2021-10-16 00:15:52 -0400

    Got the fucker working, hosting images and the feed

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
-from flask import Flask, render_template, request, url_for
-from flask.helpers import send_from_directory
+from flask import Flask, render_template, request, url_for, send_from_directory
 from unidecode import unidecode
-import dateObject
+from dateObject import db
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/')
 
-@app.route('/feed/')
+port = os.environ.get('PORT')
+if port == None:
+  port = 6942
+
+@app.route('/atom.xml')
 def feed():
-  return render_template('atom.xml')
+  return send_from_directory(app.static_folder, request.path[1:])
 
-@app.route('/images/')
-def image():
-  today = dateObject.carpeDiem()
-  month = unidecode(today.month).lower()
-  item = today.item.lower()
-  #return send_from_directory('./static/images/{}'.format(unidecode(today.month).lower()), '{}.jpg'.format(today.item.lower()))
-  return render_template('image.html', month=month, item=item)
+@app.route('/images/<image>')
+def image(image):
+  return send_from_directory(app.static_folder, request.path[1:])
 
 if __name__ == "__main__":
   #today = dateObject.carpeDiem()
-  app.run(debug=True, host="0.0.0.0", port=6942)
+  app.run(debug=True, host="0.0.0.0", port=port)
 
 #IMAGE_DIR = '/images/'
-#SERVER_HOME = 'http://{}'.format(getIpAddress())
-
-#app = Flask(__name__)
-
--- genRSS.py
+++ genRSS.py
@@ -10,6 +10,14 @@ today = carpeDiem()
 #Create the feed generator
 fg = FeedGenerator()
 
+domain = os.environ.get('DOMAIN')
+if domain == None:
+  domain = 'localhost'
+
+port = os.environ.get('PORT')
+if port == None:
+  port = '6942'
+
 def main():
   upkeepTop10()
   addDayToTop10(today)
@@ -30,9 +38,9 @@ def main():
         fe = fg.add_entry()
         fe.id(id=stamp)
         fe.title(row.formatted)
-        fe.link(href=row.item_url, rel='alternate')
+        fe.link(href='http://{domain}:{port}/images/{image}.jpg'.format(domain = domain, port = port, image = row.image), rel='alternate')
         fe.published(row.pub_date)
-        fe.content('Today is {day} {month}, the month of {month_of}. Today celebrates the {item}'.format(day=row.day, month=row.month, month_of=row.month_of, item=row.item))
+        fe.content('Today is {day} {month}, the month of {month_of}. Today celebrates {item}. You can learn more about {item} here: {item_url}'.format(day=row.day, month=row.month, month_of=row.month_of, item=row.item, item_url=row.item_url))
     
     fg.atom_file('./static/atom.xml', pretty=True)
   else:
