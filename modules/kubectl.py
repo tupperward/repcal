820aab22772ffee21eb504122476fe3fd13ffd6f
Alex Ward <alex.ward@immuta.com>
2023-05-19 01:20:54 -0400

    get host info from secret

--- modules/kubectl.py
+++ modules/kubectl.py
@@ -3,7 +3,7 @@ from kubernetes.client.rest import ApiException
 from pprint import pprint
 import os, secrets
 
-configuration = kubernetes.client.Configuration()
+configuration = kubernetes.client.Configuration(host=os.environ.get('K8S_HOST'), api_key=os.environ.get('K8S_API_KEY'))
 #configuration.api_key['authorization'] = os.environ.get('K8S_API_KEY')
 #configuration.host = os.environ.get('K8S_HOST', default="http://localhost")
 conf = kubernetes.config.load_incluster_config(client_configuration=configuration)
@@ -17,14 +17,14 @@ def create_cronjob(name: str, url: str, time_zone: str, schedule: str):
             namespace = os.environ.get('K8S_NAMESPACE')
         else:
             namespace = 'repcal'
-            
+
         salt = secrets.randbits(8)
         metadata = kubernetes.client.V1ObjectMeta(
             name=f"{name}-discord-{salt}",
             labels="discord webhook"
         )
         webhook_url = kubernetes.client.V1EnvVar(name='DISCORD_WEBHOOK_URL', value=url)
-        container = kubernetes.client.V1Container(image='tupperward/repcal:discord', env=[webhook_url], name="repcal-webhook")
+        container = kubernetes.client.V1Container(image='tupperward/repcal:discord', env=[webhook_url], name=f"{name}-discord-{salt}")
         pod_spec = kubernetes.client.V1PodSpec(containers=[container])
         pod_template = kubernetes.client.V1PodTemplateSpec(spec=pod_spec, metadata=metadata)
         job_spec = kubernetes.client.V1JobSpec(template=pod_template)
