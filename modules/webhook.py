9ab0963faf2202bfba756e77d6ecfae683cea521
Alex Ward <alex.ward@immuta.com>
2023-09-08 09:37:18 -0400

    ordinal change on webhook

--- modules/webhook.py
+++ modules/webhook.py
@@ -19,7 +19,7 @@ def get_data():
 def construct_embed(data, component = False):
   """Construct embed to send to Discord."""
   embed = Embed()
-  embed.title = f"Today is: {data['weekday'].lower()}, the {data['day']} of the {data['month']}, year {data['year_arabic']}."
+  embed.title = f"Today is: {data['weekday'].lower()}, the {data['ordinal']} of the {data['month']}, year {data['year_arabic']}."
   embed.color = Colour.green()
   embed.description = f"\n**{data['month']} is the month of {data['month_of'].lower()}.**\nToday we celebrate {data['item'].lower()}.\n\n {data['item_url'].lower()}"
   if data['month'] == "Sansculottides":
