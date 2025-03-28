f21ab03eee2a25b14cc43a48fa2b3d11cc58b5c4
Alex Ward <alex.ward@immuta.com>
2025-03-27 20:39:51 -0400

    restore to previous

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ class RepublicanDate():
     rd_date = rd.from_gregorian(time.date())
 
     self.day = rd.get_day(rd_date)
-    self.weekday = rd.get_day_in_week(rd_date)
+    self.weekday = rd.get_weekday(rd_date)
     self.month = rd.get_month(rd_date)
     self.year_arabic = rd.get_year_arabic(rd_date)
     self.year_roman = rd.get_year_roman(rd_date)
-    self.week = rd.get_week_in_year(rd_date)
+    self.week = rd.get_week_number(rd_date)
     self.month_of = None
     self.item = None
     self.item_url = None
